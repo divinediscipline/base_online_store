b521a83fb6627962d8b4b75b9d1f2a0123667e36
Divinelove Chukwuemeka <divinediscipline4real@gmail.com>
2020-08-25 03:06:40 +0100

    protect admin routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    byebug
+    # byebug
     user = User.find_by(name: params[:name])
     if user.try(:authenticate, params[:password])
       session[:user_id] = user.id
